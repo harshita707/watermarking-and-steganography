0633c2aa6a30527fb4bd464065cfa0412e310ea0
harshita707 <harshitapundir707@gmail.com>
2021-11-06 01:51:35 +0530

    update ui

--- app/src/main/java/com/application/imagesteganographyapp/MainActivity.java
+++ app/src/main/java/com/application/imagesteganographyapp/MainActivity.java
@@ -1,21 +1,22 @@
 package com.application.imagesteganographyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.application.imagesteganographyapp.Decryption.DecryptActivity;
 import com.application.imagesteganographyapp.Encryption.EncryptActivity;
 import com.application.imagesteganographyapp.Encryption.EncryptImageActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button encrypt, decrypt, encryptImg;
+    View encrypt, decrypt, encryptImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -23,28 +24,19 @@ public class MainActivity extends AppCompatActivity {
         decrypt = findViewById(R.id.decode);
         encryptImg = findViewById(R.id.encode_image);
 
-        encrypt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EncryptActivity.class);
-                startActivity(intent);
-            }
+        encrypt.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EncryptActivity.class);
+            startActivity(intent);
         });
 
-        encryptImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EncryptImageActivity.class);
-                startActivity(intent);
-            }
+        encryptImg.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EncryptImageActivity.class);
+            startActivity(intent);
         });
 
-        decrypt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DecryptActivity.class);
-                startActivity(intent);
-            }
+        decrypt.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DecryptActivity.class);
+            startActivity(intent);
         });
     }
 
